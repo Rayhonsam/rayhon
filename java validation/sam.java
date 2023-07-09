df3785ee20943037bda5ed46fa59c8aa2ec459c2
Rayhonsam <103253873+Rayhonsam@users.noreply.github.com>
2023-07-09 16:48:27 +0530

    Add files via upload

--- java validation/sam.java
+++ java validation/sam.java
@@ -1,30 +1,37 @@
+
 import java.util.*;
 import java.io.*;
-import java.util.regex.*;
 
-public class datevalidation {
+public class sam 
+{
+    class node 
+    {
+        int data;
+        node next;
+        node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    public node head=null;
+    public node tail=null;
+    public void add(int x)
+    {
+        node n=new node(x);
+        if(head==null)
+        {
+            head=n;
+            tail=n;
+            tail.next=head;
+        }
+    }
     public static void main(String[] args) {
-        int[] days={31,28,31,30,31,30,31,31,30,31,30,31};
-        String pat="2004-02-08";
-        if(Pattern.matches("[0-9]{4}-[0-9]{2}-[0-9]{2}",pat))
-        {
-            String[] arr=pat.split("-");
-            int month=Integer.parseInt(arr[1]);
-            int day=Integer.parseInt(arr[2]);
-            System.out.println(month);
-            System.out.println(day);
-            if(day<=days[month-1] && month<=12)
-            {
-                System.out.println("valid");
-            }
-            else
-            {
-                System.out.println("invalid");
-            }
-        }
-        else
-        {
-            System.out.println("invalid1");
-        }
+       sam s=new sam();
+       s.add(12);
+       s.add(134);
+       s.add(45);
+       s.delete(45);
+       s.display();
     }
 }
